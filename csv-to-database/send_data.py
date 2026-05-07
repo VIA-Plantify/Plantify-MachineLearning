0c1e5f82a9aaa670ffb8d830148269652ae642d1
Teo <teodora.cristiana.stoicescu@gmail.com>
2026-05-07 14:58:11 +0000

    Script for cvs to sql

--- csv-to-database/send_data.py
+++ csv-to-database/send_data.py
@@ -1,3 +1,5 @@
+import csv
+
 import psycopg2
 from grpc import RpcError, StatusCode
 from grpc_clients.plant_grpc_client import plant_grpc_client
@@ -20,63 +22,53 @@ print(result)
 
 #TODO get the last id from the grpc and add new data after that id, and save it in sql file; NO connection to the db
 
-# conn = psycopg2.connect(
-#     host='host.docker.internal',
-#     port=55432,
-#     database='plantify',
-#     user='dev',
-#     password='plantifydev'
-# )
-# cursor = conn.cursor()
-#
-# sql_file = open('plantify_data.sql', 'w')
-#
-# # Read CSV file and pipe it to PostgreSQL
-# with open('../mockdata/plants_users.csv', 'r') as f:
-#     cursor.copy_expert(
-#         '''COPY "Plants"("MAC", "Name", "Username", "OptimalTemperature",
-#         "OptimalAirHumidity", "OptimalSoilHumidity", "OptimalLightIntensity")
-#         FROM STDIN WITH (FORMAT CSV, HEADER)''',
-#         f
-#     )
-#
-# conn.commit()
-#
-# cursor.execute('SELECT * FROM "Plants";')
-# rows = cursor.fetchall()
-# for row in rows:
-#     sql_file.write(f"INSERT INTO \"Plants\" VALUES {row};\n")
-#
-# with open('../mockdata/sensor_datas_users.csv', 'r') as f:
-#     cursor.copy_expert(
-#         '''COPY "SensorDatas"("PlantMAC", "Temperature", "AirHumidity",
-#         "SoilHumidity", "LightIntensity", "Timestamp")
-#         FROM STDIN WITH (FORMAT CSV, HEADER)''',
-#         f
-#     )
-#
-# conn.commit()
-#
-# cursor.execute('SELECT * FROM "SensorDatas";')
-# rows = cursor.fetchall()
-# for row in rows:
-#     sql_file.write(f"INSERT INTO \"SensorDatas\" VALUES {row};\n")
-#
-# with open('../mockdata/waterings_users.csv', 'r') as f:
-#     cursor.copy_expert(
-#         '''COPY "Waterings"("PlantMAC", "PredictedFutureWaterTime", "LastWaterTime",
-#         "WaterLevel", "PumpTimeInSeconds")
-#         FROM STDIN WITH (FORMAT CSV, HEADER)''',
-#         f
-#     )
-#
-# conn.commit()
-#
-# cursor.execute('SELECT * FROM "Waterings";')
-# rows = cursor.fetchall()
-# for row in rows:
-#     sql_file.write(f"INSERT INTO \"Waterings\" VALUES {row};\n")
-#
-# sql_file.close()
-#
-# conn.close()
+sql_file = open('plantify_data.sql', 'w')
+
+# Read CSV file and pipe it to PostgreSQL
+with open('../mockdata/plants_users.csv', 'r') as f:
+    reader = csv.reader(f)
+    headers = next(reader)  # Skip header row
+    for row in reader:
+        # Format values: handle strings with quotes, numbers as-is
+        formatted_values = []
+        for value in row:
+            if value.isdigit() or (value.count('.') == 1 and value.replace('.', '').isdigit()):
+                formatted_values.append(value)  # Numbers
+            else:
+                formatted_values.append(f"'{value}'")  # Strings with quotes
+
+        values_str = ', '.join(formatted_values)
+        sql_file.write(f"INSERT INTO \"Plants\" VALUES ({values_str});\n")
+
+# Read SensorDatas CSV and generate INSERT statements
+with open('../mockdata/sensor_datas_users.csv', 'r') as f:
+    reader = csv.reader(f)
+    headers = next(reader)  # Skip header row
+    for row in reader:
+        formatted_values = []
+        for value in row:
+            if value.isdigit() or (value.count('.') == 1 and value.replace('.', '').isdigit()):
+                formatted_values.append(value)
+            else:
+                formatted_values.append(f"'{value}'")
+
+        values_str = ', '.join(formatted_values)
+        sql_file.write(f"INSERT INTO \"SensorDatas\" VALUES ({values_str});\n")
+
+# Read Waterings CSV and generate INSERT statements
+with open('../mockdata/waterings_users.csv', 'r') as f:
+    reader = csv.reader(f)
+    headers = next(reader)  # Skip header row
+    for row in reader:
+        formatted_values = []
+        for value in row:
+            if value.isdigit() or (value.count('.') == 1 and value.replace('.', '').isdigit()):
+                formatted_values.append(value)
+            else:
+                formatted_values.append(f"'{value}'")
+
+        values_str = ', '.join(formatted_values)
+        sql_file.write(f"INSERT INTO \"Waterings\" VALUES ({values_str});\n")
+
+sql_file.close()
+print("SQL script generated: plantify_data.sql")
