ba8187d2f29cdcf601981ba2f688901505f5f8cb
MarioIliescu <355142@via.dk>
2026-05-15 08:50:09 +0000

    Switching to a container function app Co-authored-by: PatrikGolian <patrik.golian888@gmail.com>

--- function_app.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import asdict
-from fastapi import FastAPI
-import azure.functions as func
-from api.plant_api_controller import router as plant_router
-
-app = FastAPI(title="Plantify ML API")
-app.include_router(plant_router)
-
-main = func.AsgiFunctionApp(app = app, http_auth_level=func.AuthLevel.ANONYMOUS)
